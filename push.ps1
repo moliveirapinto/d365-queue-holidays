43a01fcc2af0618900f43631f34adc985901170a
moliveirapinto <moliveirapinto@users.noreply.github.com>
2026-09-22 11:26:10 -0400

    UI redesign: editorial almanac theme

--- push.ps1
+++
@@ -1,5 +0,0 @@
-Set-Location "C:\Users\maoliveira\dev\d365-queue-holidays"
-git add -A
-git -c user.name="moliveirapinto" -c user.email="moliveirapinto@users.noreply.github.com" commit -q -m "v1.4.0: Queue and WFM filters in list view and calendar"
-git push -q origin main
-git rev-parse HEAD
